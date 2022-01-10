db3fc370ed32967d43d540bba3ae92ef2761e66d
micheaol <micheaol80@gmail.com>
2022-01-10 21:40:46 +0100

    Add queries to select data from the table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,11 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT name FROM animals WHERE neutered IS true AND escape_attempt < 3;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT name, escape_attempt FROM animals WHERE weight_kg > 10.5;
+SELECT *FROM animals WHERE neutered IS true;
+SELECT *FROM animals WHERE name NOT IN ('Gabumon');
+SELECT *FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+SELECT *FROM animals WHERE date_of_birth >= '12/05/2016'::date
+AND date_of_birth < '07/01/2019'::date;
