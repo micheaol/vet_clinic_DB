4e02ea1236bc1de45d0c7a8259f00e5ad6d5a52b
micheaol <micheaol80@gmail.com>
2022-01-11 20:50:53 +0100

    Update the animals table by setting the species column to digimon and pokemon with different condition

--- queries.sql
+++ queries.sql
@@ -9,3 +9,15 @@ SELECT *FROM animals WHERE name NOT IN ('Gabumon');
 SELECT *FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 SELECT *FROM animals WHERE date_of_birth >= '12/05/2016'::date
 AND date_of_birth < '07/01/2019'::date;
+SELECT COUNT(name) FROM animals;
+SELECT COUNT(escape_attempt) FROM animals WHERE escape_attempt <= 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT id, name, MAX(escape_attempt) FROM animals GROUP BY id, name;
+SELECT id, name FROM animals WHERE neutered IS false GROUP BY id, name;
+SELECT id, name FROM animals WHERE neutered IS true GROUP BY id, name;
+SELECT MIN(weight_kg) FROM animals WHERE species IN ('digimon');
+SELECT MAX(weight_kg) FROM animals WHERE species IN ('digimon');
+SELECT MIN(weight_kg) FROM animals WHERE species IN ('pokemon');
+SELECT MAX(weight_kg) FROM animals WHERE species IN ('pokemon');
+SELECT id, name, date_of_birth, SUM(escape_attempt), COUNT(escape_attempt), SUM(escape_attempt) / COUNT(escape_attempt) as average FROM animals WHERE species IN ('pokemon') AND date_of_birth >= '12/05/1990'::date AND date_of_birth <= '01/01/2021'::date GROUP BY id, name, date_of_birth;
+SELECT id, name, date_of_birth, SUM(escape_attempt), COUNT(escape_attempt), SUM(escape_attempt) / COUNT(escape_attempt) as average FROM animals WHERE species IN ('digimon') AND date_of_birth >= '12/05/1990'::date AND date_of_birth <= '01/01/2021'::date GROUP BY id, name, date_of_birth;
