c5c7ebcc871908da0bcf87e54b5f0dc38b9cb3a1
micheaol <micheaol80@gmail.com>
2022-01-13 00:18:57 +0100

    Insert the following data into the owners and species tables

--- queries.sql
+++ queries.sql
@@ -21,3 +21,11 @@ SELECT MIN(weight_kg) FROM animals WHERE species IN ('pokemon');
 SELECT MAX(weight_kg) FROM animals WHERE species IN ('pokemon');
 SELECT id, name, date_of_birth, SUM(escape_attempt), COUNT(escape_attempt), SUM(escape_attempt) / COUNT(escape_attempt) as average FROM animals WHERE species IN ('pokemon') AND date_of_birth >= '12/05/1990'::date AND date_of_birth <= '01/01/2021'::date GROUP BY id, name, date_of_birth;
 SELECT id, name, date_of_birth, SUM(escape_attempt), COUNT(escape_attempt), SUM(escape_attempt) / COUNT(escape_attempt) as average FROM animals WHERE species IN ('digimon') AND date_of_birth >= '12/05/1990'::date AND date_of_birth <= '01/01/2021'::date GROUP BY id, name, date_of_birth;
+
+SELECT animal_name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Melody Pond';
+SELECT animal_name FROM animals JOIN species ON animals.species_id = species.id WHERE species.name = 'Pokemon';
+SELECT animal_name, full_name FROM animals FULL OUTER JOIN owners ON animals.owner_id = owners.id;
+SELECT name, COUNT(species_id) FROM species JOIN animals ON animals.species_id = species.id GROUP BY species.name;
+SELECT animal_name FROM animals JOIN owners ON animals.owner_id = owners.id JOIN species ON animals.species_id = species.id WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
+SELECT full_name, animal_name, escape_attempts FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND escape_attempts = 0;
+SELECT full_name, COUNT (full_name) AS number_of_animals from owners JOIN animals ON animals.owner_id = owners.id GROUP BY owners.full_name ORDER BY number_of_animals DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -20,3 +20,24 @@ CREATE TABLE species (
 );
 
 ALTER TABLE animals DROP COLUMN species;
+
+CREATE SEQUENCE animal_id
+OWNED BY animals.id;
+
+ALTER TABLE animals
+ALTER COLUMN id SET DEFAULT nextval('animal_id');
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES owners (id);
+
+ALTER TABLE animals
+ADD species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species (id);
